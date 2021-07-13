30272a0995c31f4bbd27ba8116cd460d3f8319ad
DiegoAlessandro <diegobacigalupo8@gmail.com>
2021-07-14 01:19:28 +0900

    デプロイスクリプトの追加

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  base: '/＜REPO>/',
+  base: './',
   server: {
     port: 8080,
   },
