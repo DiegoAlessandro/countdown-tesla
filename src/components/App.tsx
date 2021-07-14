4879db0f1d655568eae42243b04384ec9564f9e4
DiegoAlessandro <diegobacigalupo8@gmail.com>
2021-07-14 23:59:50 +0900

    モーダルから車の色を選択できるようにした

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,18 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
-import {
-  CardMedia,
-  Container,
-  CssBaseline,
-  Grid,
-  IconButton,
-  makeStyles,
-  Modal,
-  Paper,
-  TextField,
-} from '@material-ui/core'
-function getModalStyle() {
-  const top = 50
-  const left = 50
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  }
-}
-import carImage from '/static/model3_white.jpeg'
+import React from 'react'
+import * as MaterialUI from '@material-ui/core'
+import carImageWhite from '/static/model3_white.jpeg'
+import carImageBlack from '/static/model3_black.jpeg'
+import carImageGray from '/static/model3_gray.jpeg'
+import carImageBlue from '/static/model3_blue.jpeg'
+import carImageRed from '/static/model3_red.jpeg'
+import paintWhiteImg from '/static/Paint_White.png'
+import paintBlackImg from '/static/Paint_Black.png'
+import paintGrayImg from '/static/Paint_MidnightSilver.png'
+import paintBlueImg from '/static/Paint_Blue.png'
+import paintRedImg from '/static/Paint_Red.png'
 import SettingsIcon from '@material-ui/icons/Settings'
-const useStyles = makeStyles((theme) => ({
+import { Avatar, Button, ButtonGroup } from '@material-ui/core'
+const useStyles = MaterialUI.makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
@@ -30,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
     position: 'absolute',
     width: 400,
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: '1px solid #000',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
@@ -50,7 +40,7 @@ const useStyles = makeStyles((theme) => ({
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: '56.25%',
   },
   margin: {
     margin: theme.spacing(1),
@@ -58,25 +48,14 @@ const useStyles = makeStyles((theme) => ({
   extendedIcon: {
     marginRight: theme.spacing(1),
   },
+  buttonBorder: { border: '0px' },
 }))
 
 const App = () => {
-  const [deliveryDateTime, setDeliveryDateTime] = useState(new Date())
-  const [nowTime, setNowTime] = useState(new Date())
-
-  /** モーダル*/
-  const classes = useStyles()
-  const [open, setOpen] = React.useState(false)
-  const [modalStyle] = React.useState(getModalStyle)
-  const handleOpen = () => {
-    setOpen(true)
-  }
-
-  const handleClose = () => {
-    setOpen(false)
-  }
-
-  const displayDateMeta = useMemo(() => {
+  /** 納車日*/
+  const [deliveryDateTime, setDeliveryDateTime] = React.useState(new Date())
+  const [nowTime, setNowTime] = React.useState(new Date())
+  const displayDateMeta = React.useMemo(() => {
     let diffMilliSeconds = deliveryDateTime.getTime() - nowTime.getTime()
     let days = Math.trunc(diffMilliSeconds / (1000 * 60 * 60) / 24)
     diffMilliSeconds = diffMilliSeconds - days * 1000 * 60 * 60 * 24
@@ -88,12 +67,67 @@ const App = () => {
     return { days, h, m, s }
   }, [deliveryDateTime, nowTime])
 
-  const refreshNowTime = useCallback(() => {
+  const refreshNowTime = React.useCallback(() => {
     setNowTime(new Date())
   }, [setNowTime])
+  /** モーダル*/
+  const getModalStyle = () => {
+    const top = 50
+    const left = 50
+
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    }
+  }
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
+  const [modalStyle] = React.useState(getModalStyle)
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  /** 色選択ボタン */
+  enum CarColor {
+    white,
+    black,
+    gray,
+    blue,
+    red,
+  }
+  const [carColor, setCarColor] = React.useState<CarColor>(CarColor.white)
+  const onClickColorButton = React.useCallback(
+    (newCarColor: CarColor) => () => {
+      localStorage.setItem('carColor', newCarColor.toString())
+      setCarColor(newCarColor)
+    },
+    [carColor, setCarColor]
+  )
+
+  const switchCarImage = React.useCallback(() => {
+    switch (carColor) {
+      case CarColor.white:
+        return carImageWhite
+      case CarColor.black:
+        return carImageBlack
+      case CarColor.gray:
+        return carImageGray
+      case CarColor.blue:
+        return carImageBlue
+      case CarColor.red:
+        return carImageRed
+      default:
+        return carImageWhite
+    }
+  }, [carColor])
 
   /** ローカルストレージ */
-  const setLocalDeliveryDateTime = useCallback(
+  const setLocalDeliveryDateTime = React.useCallback(
     (e) => {
       if (!e.target.value) return
       localStorage.setItem('deliveryDateTime', e.target.value)
@@ -103,7 +137,8 @@ const App = () => {
     [setDeliveryDateTime]
   )
 
-  const getDeliveryDateTime = useCallback(() => {
+  const getDeliveryDateTime = React.useCallback(() => {
+    /** 納車日の取得*/
     const localDeliveryDateTime = localStorage.getItem('deliveryDateTime')
     if (localDeliveryDateTime == null) {
       return false
@@ -111,11 +146,28 @@ const App = () => {
     return localDeliveryDateTime
   }, [])
 
-  useEffect(() => {
+  const getCarColor = React.useCallback(() => {
+    /** 色の取得*/
+    const localCarColor = localStorage.getItem('carColor')
+    if (localCarColor == null) {
+      return false
+    }
+
+    return localCarColor
+  }, [])
+
+  React.useEffect(() => {
+    let localCarColor = getCarColor()
+    if (localCarColor) {
+      setCarColor(Number(localCarColor))
+    }
+  }, [carColor, setCarColor])
+
+  React.useEffect(() => {
     setDeliveryDateTime(deliveryDateTime)
   }, [deliveryDateTime])
 
-  useEffect(() => {
+  React.useEffect(() => {
     let deliveryLocalDate = getDeliveryDateTime()
     if (deliveryLocalDate) {
       setDeliveryDateTime(new Date(deliveryLocalDate))
@@ -127,13 +179,16 @@ const App = () => {
 
   return (
     <>
-      <CssBaseline />
-      <Container fixed>
+      <MaterialUI.CssBaseline />
+      <MaterialUI.Container fixed>
         <div className={classes.root}>
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <Paper className={classes.paper}>
-                <CardMedia image={carImage} className={classes.media} />
+          <MaterialUI.Grid container spacing={3}>
+            <MaterialUI.Grid item xs={12}>
+              <MaterialUI.Paper className={classes.paper}>
+                <MaterialUI.CardMedia
+                  image={switchCarImage()}
+                  className={classes.media}
+                />
                 <div>
                   {deliveryDateTime
                     ? `納車日は ${deliveryDateTime.toString()}`
@@ -143,19 +198,19 @@ const App = () => {
                   {deliveryDateTime
                     ? `納車まで：${displayDateMeta.days}日 ${displayDateMeta.h}時間 ${displayDateMeta.m}分 ${displayDateMeta.s}秒`
                     : null}
-                  <IconButton
+                  <MaterialUI.IconButton
                     aria-label="delete"
                     className={classes.margin}
                     onClick={handleOpen}
                     size="small"
                   >
                     <SettingsIcon fontSize="inherit" />
-                  </IconButton>
+                  </MaterialUI.IconButton>
                 </div>
-              </Paper>
-            </Grid>
-          </Grid>
-          <Modal
+              </MaterialUI.Paper>
+            </MaterialUI.Grid>
+          </MaterialUI.Grid>
+          <MaterialUI.Modal
             open={open}
             onClose={handleClose}
             aria-labelledby="simple-modal-title"
@@ -164,7 +219,7 @@ const App = () => {
             <div style={modalStyle} className={classes.modalPaper}>
               <h2 id="simple-modal-title">納車日を入力</h2>
               <form className={classes.container} noValidate>
-                <TextField
+                <MaterialUI.TextField
                   id="datetime-local"
                   label="納車日"
                   type="datetime-local"
@@ -175,10 +230,48 @@ const App = () => {
                   }}
                 />
               </form>
+              <div>
+                <h2>色</h2>
+                <ButtonGroup
+                  size="small"
+                  aria-label="small outlined button group"
+                >
+                  <Button
+                    startIcon={<Avatar src={paintWhiteImg} />}
+                    className={classes.buttonBorder}
+                    onClick={onClickColorButton(CarColor.white)}
+                    value={'white'}
+                  />
+                  <Button
+                    startIcon={<Avatar src={paintBlackImg} />}
+                    className={classes.buttonBorder}
+                    onClick={onClickColorButton(CarColor.black)}
+                    value={'black'}
+                  />
+                  <Button
+                    startIcon={<Avatar src={paintGrayImg} />}
+                    className={classes.buttonBorder}
+                    onClick={onClickColorButton(CarColor.gray)}
+                    value={'gray'}
+                  />
+                  <Button
+                    startIcon={<Avatar src={paintBlueImg} />}
+                    className={classes.buttonBorder}
+                    onClick={onClickColorButton(CarColor.blue)}
+                    value={'blue'}
+                  />
+                  <Button
+                    startIcon={<Avatar src={paintRedImg} />}
+                    className={classes.buttonBorder}
+                    onClick={onClickColorButton(CarColor.red)}
+                    value={'red'}
+                  />
+                </ButtonGroup>
+              </div>
             </div>
-          </Modal>
+          </MaterialUI.Modal>
         </div>
-      </Container>
+      </MaterialUI.Container>
     </>
   )
 }
